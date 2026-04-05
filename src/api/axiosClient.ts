8054801e1932e4d6c721334063dfed7fb31e613a
Nguyễn Quốc Huy <nguyenquochuyc7@gmail.com>
2026-04-05 20:27:44 +0700

    fix

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -5,7 +5,7 @@ const axiosClient = axios.create({
 })
 
 axiosClient.interceptors.request.use((config) => {
-
+    config.headers["ngrok-skip-browser-warning"] = "true"
     const token = localStorage.getItem("token")
 
     if (token) {
